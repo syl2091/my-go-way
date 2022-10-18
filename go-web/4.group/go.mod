ee6467592c455397363578672c25462783133c8a
onthroad <164378335@qq.com>
2022-10-18 10:31:14 +0800

    go-web

---
+++ go-web/4.group/go.mod
@@ -0,0 +1,10 @@
+module example
+
+
+require (
+	lege v0.0.1
+)
+
+replace (
+	lege => ./lege
+)
---
+++ go-web/4.group/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"lege"
+	"net/http"
+)
+
+func main() {
+	r := lege.New()
+	r.GET("/index", func(c *lege.Context) {
+		c.HTML(http.StatusOK, "<h1>Index page</h1>")
+	})
+	v1 := r.Group("/v1")
+
+	{
+		v1.GET("/", func(c *lege.Context) {
+			c.HTML(http.StatusOK, "<h1>hello lege<h1>")
+		})
+
+		v1.GET("/hello", func(c *lege.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *lege.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *lege.Context) {
+			c.JSON(http.StatusOK, lege.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
+	r.Run(":9999")
+}
